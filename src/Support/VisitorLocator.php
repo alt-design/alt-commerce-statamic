7b77c9b590dd1f2af51fcea4e1dc313e4ca1f3a5
James Banton <james-banton@live.co.uk>
2025-03-19 17:47:00 +0000

    Handle exceptions in VisitorLocator retrieve method
    Add a try-catch block to handle potential errors when retrieving location data. Log any exceptions for better debugging and ensure the method safely returns null in case of a failure.

--- src/Support/VisitorLocator.php
+++ src/Support/VisitorLocator.php
@@ -3,14 +3,19 @@
 namespace AltDesign\AltCommerceStatamic\Support;
 
 use AltDesign\AltCommerce\Support\Location;
+use Illuminate\Support\Facades\Log;
 
 class VisitorLocator implements \AltDesign\AltCommerce\Contracts\VisitorLocator
 {
 
     public function retrieve(): Location|null
     {
-        if ($position = \Stevebauman\Location\Facades\Location::get()) {
-            return new Location(countryCode: $position->countryCode, currency: $position->currencyCode);
+        try {
+            if ($position = \Stevebauman\Location\Facades\Location::get()) {
+                return new Location(countryCode: $position->countryCode, currency: $position->currencyCode);
+            }
+        } catch (\Throwable $e) {
+            Log::error($e);
         }
 
         return null;
