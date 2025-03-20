160d4b0d2eeb13c1e5cbd9f037c13e56ac1752a8
James Banton <james-banton@live.co.uk>
2025-03-20 09:17:02 +0000

    Update IP lookup in VisitorLocator to use ip-api.com
    Replaces the stevebauman/location package with a direct integration with ip-api.com for IP-based geolocation. Adds caching with a 1-day expiry to reduce redundant API calls and improve performance. Improves error handling and logging for failed API responses.

--- src/Support/VisitorLocator.php
+++ src/Support/VisitorLocator.php
@@ -3,6 +3,8 @@
 namespace AltDesign\AltCommerceStatamic\Support;
 
 use AltDesign\AltCommerce\Support\Location;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
 class VisitorLocator implements \AltDesign\AltCommerce\Contracts\VisitorLocator
@@ -10,14 +12,40 @@ class VisitorLocator implements \AltDesign\AltCommerce\Contracts\VisitorLocator
 
     public function retrieve(): Location|null
     {
+        $ip = request()->ip();
+        if (empty($ip)) {
+            return null;
+        }
+
+        $cacheKey = "alt-commerce-ip-lookup-$ip";
+        if ($response = Cache::get($cacheKey)) {
+            return $response;
+        }
+
         try {
-            if ($position = \Stevebauman\Location\Facades\Location::get()) {
-                return new Location(countryCode: $position->countryCode, currency: $position->currencyCode);
+            $response = Http::get('http://ip-api.com/json/', [
+                'fields' => 'status,countryCode,currency',
+            ]);
+
+            $content = json_decode($response->getBody()->getContents());
+            if (!$response->ok() || $content->status !== 'success') {
+                throw new \Exception('Invalid response from ip-api.com', [
+                    'ip' => $ip,
+                    'status' => $response->getStatusCode(),
+                    'content' => $content,
+                ]);
             }
+
+            return Cache::remember($cacheKey, now()->addDay(), fn() => new Location(
+                countryCode: $content->countryCode,
+                currency: $content->currency
+            ));
+
         } catch (\Throwable $e) {
             Log::error($e);
         }
 
         return null;
+
     }
 }
