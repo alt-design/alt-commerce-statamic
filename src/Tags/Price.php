2df3bfe3008c76acdbcfa4e9a5e17e318f3f3c46
James Banton <james-banton@live.co.uk>
2025-04-01 16:31:59 +0100

    dev

--- src/Tags/Price.php
+++ src/Tags/Price.php
@@ -2,44 +2,20 @@
 
 namespace AltDesign\AltCommerceStatamic\Tags;
 
-use AltDesign\AltCommerce\Contracts\BasketRepository;
 use AltDesign\AltCommerce\Contracts\VisitorLocator;
-use AltDesign\AltCommerce\Exceptions\PriceNotAvailableException;
 use AltDesign\AltCommerceStatamic\Commerce\Product\PriceCollectionFactory;
 use Statamic\Tags\Tags;
 
 class Price extends Tags
 {
     public function __construct(
-        protected BasketRepository       $basketRepository,
         protected PriceCollectionFactory $priceCollectionFactory,
         protected VisitorLocator         $visitorLocator,
     )
     {
     }
 
-    public function index()
-    {
-        try {
-            $currency = $this->basketRepository->get()->currency;
 
-            //$collection = $this->priceCollectionFactory->create($this->context->get('pricing'));
-
-            //$amount = $collection->currency($currency);
-
-            return [
-                'currency' => $currency,
-                'amount' => 60,
-                'formatted' => $this->format(
-                    currency: $currency,
-                    amount: 60,
-                )
-            ];
-
-        } catch(PriceNotAvailableException) {
-            return [];
-        }
-    }
 
     public function format(string|null $currency = null, int|null $amount = null): string
     {
