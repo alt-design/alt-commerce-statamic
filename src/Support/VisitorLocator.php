52e168f489eef08ca78050fd1a8212d680f82885
James Banton <james-banton@live.co.uk>
2025-05-21 13:41:02 +0100

    Refactor VisitorLocator to use request data directly.
    Simplified the retrieve method by removing external IP-based lookup logic and HTTP requests. The method now directly retrieves visitor's country and currency from the request, reducing dependencies and potential points of failure.

--- src/Support/VisitorLocator.php
+++ src/Support/VisitorLocator.php
@@ -12,40 +12,15 @@ class VisitorLocator implements \AltDesign\AltCommerce\Contracts\VisitorLocator
 
     public function retrieve(): Location|null
     {
-        $ip = request()->ip();
-        if (empty($ip)) {
+        $currency = request('visitor-currency');
+        $country = request('visitor-country');
+
+        if (empty($currency) || empty($country)) {
             return null;
         }
-
-        $cacheKey = "alt-commerce-ip-lookup-$ip";
-        if ($response = Cache::get($cacheKey)) {
-            return $response;
-        }
-
-        try {
-            $response = Http::get('http://ip-api.com/json/', [
-                'fields' => 'status,countryCode,currency',
-            ]);
-
-            $content = json_decode($response->getBody()->getContents());
-            if (!$response->ok() || $content->status !== 'success') {
-                throw new \Exception('Invalid response from ip-api.com', [
-                    'ip' => $ip,
-                    'status' => $response->getStatusCode(),
-                    'content' => $content,
-                ]);
-            }
-
-            return Cache::remember($cacheKey, now()->addDay(), fn() => new Location(
-                countryCode: $content->countryCode,
-                currency: $content->currency
-            ));
-
-        } catch (\Throwable $e) {
-            Log::error($e);
-        }
-
-        return null;
-
+        return new Location(
+            countryCode: $country,
+            currency: $currency,
+        );
     }
 }
